ca6618d8073c47ebcaa2dd5bc975016d58939f0c
landrich <landrich86@gmail.com>
2024-01-24 00:09:59 +0900

    upload 240124

---
+++ Programmers/코딩테스트_입문_Lv.0/TheNumberComesNext.java
@@ -0,0 +1,38 @@
+/*
+[다음에 올 숫자]
+
+* 문제 설명
+  - 등차수열 혹은 등비수열 common이 매개변수로 주어질 때, 마지막 원소 다음으로 올 숫자를
+    return 하도록 solution 함수를 완성해보세요.
+
+* 제한사항
+  - 2 < common의 길이 < 1,000
+  - -1,000 < common의 원소 < 2,000
+  - common의 원소는 모두 정수입니다.
+  - 등차수열 혹은 등비수열이 아닌 경우는 없습니다.
+  - 등비수열인 경우 공비는 0이 아닌 정수입니다.
+ */
+
+class Solution {
+    public int solution(int[] common) {
+        if (common[1] + (common[1] - common[0]) == common[2]) {
+            return common[common.length - 1] + (common[1] - common[0]);
+        }
+        if (common[1] * (common[1] / common[0]) == common[2]) {
+            return common[common.length - 1] * (common[1] / common[0]);
+        }
+
+        return -1;
+    }
+}
+
+public class TheNumberComesNext {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.solution(new int[]{-1, 2, -4}));
+    }
+}
+
+/*
+# Another Answer
+ */

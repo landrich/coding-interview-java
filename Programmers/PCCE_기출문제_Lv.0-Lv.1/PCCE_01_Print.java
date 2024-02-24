1fef39277095f5145c8974b4c088f73587d0c7e6
landrich <landrich86@gmail.com>
2024-02-24 10:51:00 +0900

    upload 240224

---
+++ Programmers/PCCE_기출문제_Lv.0-Lv.1/PCCE_01_Print.java
@@ -0,0 +1,15 @@
+/*
+[PCCE_01_Print]
+ */
+
+public class PCCE_01_Print {
+    public static void main(String[] args) {
+        String msg = "Spring is beginning";
+        int val1 = 3;
+        String val2 = "3";
+
+        System.out.println(msg);
+        System.out.println(val1 + 10);
+        System.out.println(val2 + "10");
+    }
+}
